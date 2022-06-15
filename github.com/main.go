e967be73d03212a8ad6d4401b9ebd812df8e28f7
usman <musman@gomarkho.com>
2022-06-15 08:24:30 -0700

    Update to map two route getContact CreateContact

--- github.com/main.go
+++ github.com/main.go
@@ -1,40 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func getHome(res http.ResponseWriter, req *http.Request) {
-	fmt.Println("Inside getHome route", req.Method)
-	switch req.Method {
-	case "GET":
-		result := "Home Page\n"
-		io.WriteString(res, result)
-		return
-	case "POST":
-		if err := req.ParseForm(); err != nil {
-			fmt.Fprintf(res, "ParseForm() err: %v", err)
-			return
-		}
-		fmt.Fprintf(res, "Post from website! r.PostFrom = %v\n", req.PostForm)
-		io.WriteString(res, req.FormValue("name"))
-		return
-	default:
-		result := "Only suppoet Get and Post method"
-		fmt.Println(result)
-		io.WriteString(res, result)
-		return
+type Contact struct {
+	Name string `json:Name`
+	Age  int64  `json:Age`
+}
+
+func getContacts(res http.ResponseWriter, req *http.Request) {
+	fmt.Println("Inside index method")
+	result := "Get Contact method\n"
+	io.WriteString(res, result)
+}
+
+func createContact(res http.ResponseWriter, req *http.Request) {
+	reqBody, _ := ioutil.ReadAll(req.Body)
+	var contact Contact
+	json.Unmarshal(reqBody, &contact)
+	json.NewEncoder(res).Encode(contact)
+	newContact, err := json.Marshal(reqBody)
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println(err)
+	} else {
+		fmt.Println(string(newContact))
 	}
 }
 
 func main() {
-	fmt.Println("Starting server")
-	http.HandleFunc("/", getHome)
-	err := http.ListenAndServe(":3333", nil)
+	fmt.Println("Server starting")
+	r := mux.NewRouter().StrictSlash(true)
+
+	// routes
+	r.HandleFunc("/getContacts", getContacts).Methods("GET")
+	r.HandleFunc("/createContact", createContact).Methods("POST")
+
+	// start server
+	err := http.ListenAndServe(":3333", r)
 	if err != nil {
 		log.Fatal(err)
+	} else {
+		fmt.Println("Server started")
 	}
 }
